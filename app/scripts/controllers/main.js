ecc8e8438f8c3637be7f76f68c8b36b3b5a4d6d0
mayukas <mayuka@stanford.edu>
2017-08-03 16:29:05 -0400

    Working back-end search, filters by all params with url storage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,8 +14,17 @@ angular.module('localResourcesApp')
 
     $scope.user = {};
     $scope.user.loadingLoc = false;
-    $scope.user.byLegal = false;
-    if(!$location.search().type) $location.search('type', 'community');
+    //$scope.user.byLegal = false;
+    $scope.user.org_type = ['legal', 'community', 'govt'];
+    $scope.user.userTags = [];
+    $scope.user.housingType = '';
+    $scope.user.housingCourtStatus = false;
+    $scope.user.eligibilityTags = [];
+    $scope.user.issueTags = [];
+    $scope.user.finishedIntake = false;
+    $scope.user.editSearchOpen = false; //change
+    $scope.user.currentIntake = 1;
+    //if(!$location.search().showResults) $location.search('showResults', false);
 
     if(!$window.Geocoder) {
       // $log.info('ERROR: no geocoder set.');
@@ -33,11 +42,15 @@ angular.module('localResourcesApp')
     $scope.searchAddr = function() {
       // $log.info('SEARCHED=' + $scope.user.address);
       // $window.Geocoder.geocode({ 'address': $scope.user.address }, function(results, status) {
+
       $window.Geocoder.geocode({
         address: $scope.user.address,
         bounds: boundsNYC
       }, function(results, status) {
         if (status === google.maps.GeocoderStatus.OK) {
+          $scope.user.editSearchOpen = true;
+          console.log('edit search open is ', $scope.user.editSearchOpen);
+
           $scope.user.lat = results[0].geometry.location.lat();
           $scope.user.lng = results[0].geometry.location.lng();
           $scope.error = false;
@@ -52,7 +65,6 @@ angular.module('localResourcesApp')
             });
           }
           ga('send', 'event', 'Map', 'search-found', 'Initial');
-          $scope.update();
         } else {
           $scope.error = true;
           $scope.$apply();
@@ -71,6 +83,7 @@ angular.module('localResourcesApp')
         var latlng = { lat: _lat, lng: _lng };
         $window.Geocoder.geocode({ 'location': latlng }, function(results, status) {
           if (status === google.maps.GeocoderStatus.OK) {
+            $scope.user.editSearchOpen = true;
             $scope.user.lat = results[0].geometry.location.lat();
             $scope.user.lng = results[0].geometry.location.lng();
             $scope.error = false;
@@ -84,7 +97,6 @@ angular.module('localResourcesApp')
                 found: results[0].formatted_address
               });
             }
-            $scope.update();
           } else {
             $scope.error = true;
             $scope.user.loadingLoc = false;
@@ -95,29 +107,72 @@ angular.module('localResourcesApp')
       });
     };
 
-    $scope.toggleOrgType = function(byLegal) {
-      if(byLegal) {
+    $scope.toggleOrgType = function(type) {
+      /*if(byLegal) {
         $location.search('type', 'legal');
       } else {
         $location.search('type', 'community');
       }
-      $scope.user.byLegal = byLegal;
+      $scope.user.byLegal = byLegal;*/
+      $scope.updateTags($scope.user.org_type,type);
+      $location.search('type', $scope.user.org_type);
       $scope.update();
     };
 
+    $scope.updateHousingType = function(tag){
+      $location.search('housing', tag);
+      $scope.user.housingType = tag;
+      console.log('user tags: ', $scope.user.userTags);
+    }
+    $scope.updateHousingCourt = function(status){
+      $location.search('court', status);
+      $scope.user.housingCourtStatus = status;
+      console.log('housing court status: ', $scope.user.housingCourtStatus);
+      //$scope.update();
+    }
 
+    $scope.containsTag = function(arr, tag){
+      return (arr.indexOf(tag) > -1);
+    }
 
+    $scope.updateTags = function(arr, tag){
+      if ($scope.containsTag(arr, tag)){
+        arr.splice(arr.indexOf(tag), 1);
+        console.log('removed tag: ', tag);
+      }
+      else{
+        arr.push(tag);
+      }
+    }
+
+    $scope.updateEligibilityTags = function(tag){
+      $scope.updateTags($scope.user.eligibilityTags, tag);
+      $location.search('you', $scope.user.eligibilityTags);
+    }
+    $scope.updateIssueTags = function(tag){
+      $scope.updateTags($scope.user.issueTags, tag);
+      $location.search('issues', $scope.user.issueTags);
+    }
+//Update function is called when intake screener is completed or edited, to make a new query
     $scope.update = function() {
+      $scope.user.finishedIntake = true;
+      $scope.user.editSearchOpen = false;
+      console.log('edit search open? ', $scope.user.editSearchOpen);
+      //document.getElementById("housingType").innerHTML = $scope.user.housingType;
       var lat = $scope.user.lat;
       var lng = $scope.user.lng;
-      $scope.updateCartoMap(lat, lng, $scope.user.byLegal);
-      $scope.updateCartoList(lat, lng, $scope.user.byLegal);
+      $scope.user.userTags = [$scope.user.housingType].concat( $scope.user.issueTags, $scope.user.eligibilityTags);
+      console.log('USER TAGS ARRAY: ', $scope.user.userTags);
+      $scope.updateCartoMap(lat, lng, $scope.user.org_type, $scope.user.housingCourtStatus, $scope.user.userTags);
+      $scope.updateCartoList(lat, lng, $scope.user.org_type, $scope.user.housingCourtStatus, $scope.user.userTags);
+      console.log('will query for housing type ', $scope.user.housingType, ' and status in housing court ', $scope.user.housingCourtStatus, ' specializing in ', $scope.user.eligibilityTags);
     };
 
-    $scope.updateCartoList = function(lat, lng, orgType) {
-      CartoDB.queryByLatLng(lat, lng, orgType)
-        .done(function (data) {
+    $scope.updateCartoList = function(lat, lng, orgType, housingCourtStatus, userTags) {
 
+      CartoDB.queryByLatLng(lat, lng, orgType, housingCourtStatus, userTags)
+        .done(function (data) {
+          console.log('FINISHED LIST');
           if(data.rows.length == 0) {
             // $log.info('NO RESULTS=' + $scope.user.address);
             if(typeof Rollbar !== "undefined") {
@@ -139,6 +194,10 @@ angular.module('localResourcesApp')
             console.log("errors:" + errors);
         });
       };
+
+    var currentIntakeQuestion = function(num){
+      return ($scope.user.currentIntake == num);
+    }
 
     var getUserBorough = function(addr) {
       if(/Brooklyn/i.test(addr)) return 'Brooklyn';
@@ -171,23 +230,44 @@ angular.module('localResourcesApp')
       }
     };
 
+    $scope.toArray = function(object){
+      if (typeof object == "string") object = [object];
+      return object;
+    }
+
+    if ($location.search().showResults) $location.search('showResults', !$scope.user.finishedIntake && $scope.user.editSearchOpen);
+
+//Load results from url based on pre-existing search parameters
     if($location.search().search) {
-      console.log('search', $location.search().search);
+      console.log('ADDRESS', $location.search().search);
       $scope.user.address = $location.search().search;
       $scope.searchAddr();
     }
     if($location.search().type) {
-      switch($location.search().type) {
-        case 'legal':
-          $scope.user.byLegal = true;
-          break;
-        case 'community':
-          $scope.user.byLegal = false;
-          break;
-       default:
-         $scope.user.byLegal = false;
-         break;
-      }
+      $scope.user.org_type = $scope.toArray($location.search().type);
+      console.log('ORG TYPE', $scope.user.org_type);
     }
+    if($location.search().housing){
+      $scope.user.housingType = $location.search().housing;
+      console.log('HOUSING TYPE', $scope.user.housingType);
+    }
+    if($location.search().court){
+      $scope.user.housingCourtStatus = $location.search().court;
+      console.log('COURT STATUS', $scope.user.housingCourtStatus);
+
+    }
+
+    if($location.search().you){
+      //toArray: If single search param is encoded as a string instead of array, convert to array before assigning to eligibilityTags
+      $scope.user.eligibilityTags = $scope.toArray($location.search().you);
+      console.log('ELIGIBILITY', $scope.user.eligibilityTags);
+    }
+    if($location.search().issues){
+      $scope.user.issueTags = $scope.toArray($location.search().issues);
+      console.log('ISSUE TAGS', $scope.user.issueTags);
+    }
+
+
+
 
 }]);
--- app/scripts/services/cartodb.js
+++ app/scripts/services/cartodb.js
@@ -25,14 +25,13 @@ angular.module('localResourcesApp')
 
     /* public functions */
     return {
-      queryByLatLng: function(lat, lng, orgType) {
+      queryByLatLng: function(lat, lng, orgType, housingCourt, userTags) {
 
         //var boroughString = borough ? '' : 'NOT';
-        var orgString = orgType ? 'legal' : 'community';
-        var housingCourt = false;
+        var orgString = orgType.toString();
+        console.log(orgString, ' <<ORG TYPE STRING');
+        //var housingCourt = false;
         var housingCourtStatusQuery = housingCourt ? ' AND loc.housing_court = true' : '';
-
-        var userTags = ['seniors', 'disability', 'health', 'eviction', 'income'];
         var userTagString = userTags.toString();
         console.log(userTagString, ' <<USER TAG STRING');
         // var query = "SELECT *, row_number() OVER (ORDER BY dist) as rownum FROM ( SELECT bcl.organization, bcl.contact_information, bcl.address, bcl.services, round( (ST_Distance( ST_GeomFromText('Point(" + lng + " " + lat + ")', 4326)::geography, bcl.the_geom::geography ) / 1609)::numeric, 1 ) AS dist FROM brooklyn_cbos_locations AS bcl, brooklyn_cbos AS bc WHERE ST_Intersects( ST_GeomFromText( 'Point(" + lng + " " + lat + ")', 4326 ), bc.the_geom ) AND bc.cartodb_id = bcl.cartodb_id AND bc.service_area_type " + boroughString + " IN ('borough') ORDER BY dist ASC ) T";
@@ -40,9 +39,9 @@ angular.module('localResourcesApp')
         //var query = "SELECT *, row_number() OVER (ORDER BY dist) as rownum FROM ( SELECT loc.organization, loc.contact_information, loc.address, loc.services, round( (ST_Distance( ST_GeomFromText('Point(" + lng + " " + lat + ")', 4326)::geography, loc.the_geom::geography ) / 1609)::numeric, 1 ) AS dist FROM nyc_cbos_locations_2 AS loc, nyc_cbos_service_areas AS sa WHERE ST_Intersects( ST_GeomFromText( 'Point(" + lng + " " + lat + ")', 4326 ), sa.the_geom ) AND loc.organization = sa.organization AND loc.org_type IN ('" + orgString + "') ORDER BY dist ASC ) T LIMIT 10";
 
 
-        var query = "SELECT *, row_number() OVER (ORDER BY dist ASC, score DESC) as rownum FROM ( SELECT loc.organization, loc.contact_information, loc.address, loc.services, loc.requirements, loc.housing_court, char_length(tags) AS score, round( (ST_Distance( ST_GeomFromText('Point(" + lng + " " + lat + ")', 4326)::geography, loc.the_geom::geography ) / 1609)::numeric, 1 ) AS dist FROM nyc_cbos_locations_2 AS loc, nyc_cbos_service_areas AS sa WHERE ST_Intersects( ST_GeomFromText( 'Point(" + lng + " " + lat + ")', 4326 ), sa.the_geom ) AND (position(loc.requirements in '" +userTagString+ "') != 0 OR loc.requirements = '') AND loc.organization = sa.organization "+ housingCourtStatusQuery + " AND loc.org_type IN ('" + orgString + "') ) T LIMIT 5"; //currently only works for a single requirement
+        var query = "SELECT *, row_number() OVER (ORDER BY dist ASC, score DESC) as rownum FROM ( SELECT loc.organization, loc.contact_information, loc.address, loc.services, loc.requirements, loc.housing_court, char_length(tags) AS score, round( (ST_Distance( ST_GeomFromText('Point(" + lng + " " + lat + ")', 4326)::geography, loc.the_geom::geography ) / 1609)::numeric, 1 ) AS dist FROM nyc_cbos_locations_FINAL AS loc, nyc_cbos_service_areas_copy_new_entries AS sa WHERE ST_Intersects( ST_GeomFromText( 'Point(" + lng + " " + lat + ")', 4326 ), sa.the_geom ) AND (position(loc.requirements in '" +userTagString+ "') != 0 OR loc.requirements = '') AND loc.organization = sa.organization "+ housingCourtStatusQuery + " AND (position(loc.org_type in '" + orgString + "') != 0 )) T LIMIT 10"; //currently only works for a single requirement
 
-        console.log('QUERY: ', query);
+        console.log('LIST QUERY: ', query);
         return cartoSQL.execute(query);
       },
       getSQL: function() { return cartoSQL; }
